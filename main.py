623b95558fed50ae06b1477d021bc8ea2533acd2
chtholine <oleg123qwe@gmail.com>
2023-11-03 22:12:43 +0200

    first working bot

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import asyncio
 import logging
+
 import replicate
+from aiogram.filters import Command
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
 
 load_dotenv(dotenv_path=".env")
 
@@ -15,12 +16,38 @@ bot = Bot(token=token)
 dp = Dispatcher()
 
 
-@dp.message
-async def echo(message: types.Message):
-    await bot.send_message(
-        chat_id=message.chat.id,
-
+# -- LLM API -- #
+async def llm_prompt(prompt):
+    output = replicate.run(
+        "joehoover/zephyr-7b-alpha:14ec63365a1141134c41b652fe798633f48b1fd28b356725c4d8842a0ac151ee",
+        input={
+            "system_prompt": "You are writing high quality documentation for code.",
+            "min_new_tokens": 1,
+            "max_new_tokens": 500,
+            "temperature": 0.75,
+            "top_p": 0.9,
+            "top_k": 50,
+            "prompt": prompt,  # Use the user's message as the prompt
+        }
     )
+
+    return "".join(str(item) for item in output)
+
+
+# -- BOT Handlers -- #
+@dp.message(Command("start"))
+async def handle_start(message: types.Message):
+    await message.answer(text='''
+Greetings!
+I'm a bot that helps you document your code.
+Please send code as plain text or a file.
+    ''')
+
+
+@dp.message()
+async def echo(message: types.Message):
+    llm_response = await llm_prompt(message.text)
+    await message.reply(llm_response)
 
 
 async def main():
@@ -30,25 +57,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-# -- LLM API -- #
-
-def llm():
-    output = replicate.run(
-        "joehoover/zephyr-7b-alpha:14ec63365a1141134c41b652fe798633f48b1fd28b356725c4d8842a0ac151ee",
-        input={"system_prompt": "You are writing high quality documentation for code.",
-               "min_new_tokens": 1,
-               "max_new_tokens": 500,
-               "temperature": 0.75,
-               "top_p": 0.9,
-               "top_k": 50,
-               "prompt": '''
-                    
-               '''}
-    )
-    # The joehoover/zephyr-7b-alpha model can stream output as it's running.
-    # The predict method returns an iterator, and you can iterate over that output.
-    for item in output:
-        # https://replicate.com/joehoover/zephyr-7b-alpha/versions/14ec63365a1141134c41b652fe798633f48b1fd28b356725c4d8842a0ac151ee/api#output-schema
-        print(item, end="")
+    
