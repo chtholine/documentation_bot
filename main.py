e41c52fb3aae014fb21a3835581711237dcb7b85
chtholine <oleg123qwe@gmail.com>
2023-11-04 16:39:01 +0200

    added document type

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import replicate
 from aiogram.filters import Command
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
+from typing import BinaryIO
 
 load_dotenv(dotenv_path=".env")
 
@@ -14,20 +15,29 @@ token: str = os.getenv("TOKEN")
 
 bot = Bot(token=token)
 dp = Dispatcher()
+MyBinaryIO = BinaryIO
+my_object = MyBinaryIO()
+
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5 MB
 
 
 # -- LLM API -- #
-async def llm_prompt(prompt):
+async def llm_prompt(prompt: str):
     output = replicate.run(
         "joehoover/zephyr-7b-alpha:14ec63365a1141134c41b652fe798633f48b1fd28b356725c4d8842a0ac151ee",
         input={
-            "system_prompt": "You are writing high quality documentation for code.",
+            "system_prompt": '''
+You are writing high quality documentation for code you are provided with. 
+Create comprehensive code documentation to help developers understand the project's structure, functionality, and usage.
+The documentation should cover key aspects of the code.
+This documentation will serve as a valuable resource for both new and existing developers working on the project.
+            ''',
             "min_new_tokens": 1,
             "max_new_tokens": 500,
             "temperature": 0.75,
             "top_p": 0.9,
             "top_k": 50,
-            "prompt": prompt,  # Use the user's message as the prompt
+            "prompt": prompt,
         }
     )
 
@@ -35,7 +45,7 @@ async def llm_prompt(prompt):
 
 
 # -- BOT Handlers -- #
-@dp.message(Command("start"))
+@dp.message(Command("start", "help"))
 async def handle_start(message: types.Message):
     await message.answer(text='''
 Greetings!
@@ -45,10 +55,23 @@ Please send code as plain text or a file.
 
 
 @dp.message()
-async def echo(message: types.Message):
+async def handle_message(message: types.Message):
     processing_msg = await message.reply("Processing...")
-    llm_response = await llm_prompt(message.text)
-    await bot.edit_message_text(chat_id=processing_msg.chat.id, message_id=processing_msg.message_id, text=llm_response)
+    if message.document:
+        file_info = message.document
+        if file_info.file_size <= MAX_FILE_SIZE:
+            file_content = await bot.download(file_info)
+            file_content_text = file_content.read().decode("utf-8")
+            print(file_content_text)
+            llm_response = await llm_prompt(file_content_text)
+            await bot.edit_message_text(chat_id=processing_msg.chat.id, message_id=processing_msg.message_id,
+                                        text=llm_response)
+        else:
+            await message.reply("Please upload a file less than 5mb.")
+    if message.text:
+        llm_response = await llm_prompt(message.text)
+        await bot.edit_message_text(chat_id=processing_msg.chat.id, message_id=processing_msg.message_id,
+                                    text=llm_response)
 
 
 async def main():
@@ -58,4 +81,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-    
